1647476cb62cba0d0abcadfdf1276baeae412e50
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-04 18:54:11 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import json
 import sqlite3
+
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
