f0d3206e845a8ed394f16730dba1dfc00123fcdc
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-04 20:04:57 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -23,11 +23,10 @@ def carre(val_user):
 
 @app.route('/somme/<int:val_user>/<int:val_user2>')
 def somme(val_user,val_user2):
-def pair(val_user,val_user2):
-if (val_user + val_user2) % 2 == 0:
-pair = True
-else:
-pair = False
+    if (val_user + val_user2) % 2 == 0:
+    pair = "paire"
+    else:
+    pair = "impaire"
     return "<h2>La somme des 2 valeurs est : </h2>" + str(val_user + val_user2)+"<h2>et est</h2>"+ str(pair)
                                                                                                                
 if __name__ == "__main__":
