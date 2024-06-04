77a111e3d27c84cdce9c3cf6defcd26ee3ff7086
boscoshot <113295603+boscoshot@users.noreply.github.com>
2024-06-04 19:42:16 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,9 +2,8 @@ from flask import Flask
 from flask import render_template
 from flask import json
 import sqlite3
-
                                                                                                                                        
-app = Flask(__name__)
+app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
@@ -13,18 +12,6 @@ def hello_world():
 @app.route('/exercices/')
 def exercices():
     return render_template('exercices.html')
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return render_template("contact.html")
-
-@app.route('/calcul_carre/<int:val_user>')
-def carre(val_user):
-    return "<h2>Le carré de votre valeur est : </h2>" + str(val_user * val_user)
-
-@app.route('/somme/<int:val_user>/<int:val_user2>')
-def somme(val_user,val_user2):
-      return "<h2>La somme des 2 valeurs saisies est : </h2>" + str(val_user + val_user2)"        
                                                                                                                
 if __name__ == "__main__":
   app.run(debug=True)
